794c03d589feb21237323cc2b1cdb5adb3a11310
classmate-Han <2207946194@qq.com>
2023-10-10 23:16:28 +0800

    实现向右旋转k次数组

---
+++ time&space complexity/time&space complexity/text3.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//实现向右旋转k次数组
+// 旋转一次[1,2,3,4,5]变为[5,1,2,3,4]
+
+//三段逆置
+void reserve(int* a, int left, int right)
+{
+	while (left < right) {
+		int tmp = a[left];
+		a[left] = a[right];
+		a[right] = tmp;
+		left++;
+		right--;
+	}
+}
+
+void rotate(int* nums, int numsSize, int k)
+{
+	if (k > numsSize)
+		k %= numsSize;
+	reserve(nums, numsSize - k, numsSize);
+	reservr(nums, 0, numsSize - k - 1);
+}
+
+//空间换时间，复制
+void rotate_1(int* nums, int numsSize, int k)
+{
+	if (k > numsSize)
+		k %= numsSize;
+	int* tmp = (int*)malloc(sizeof(int) * numsSize);
+	memcpy(tmp + k, nums, sizeof(int) * (numsSize - k));
+	memcpy(tmp, nums + numsSize - k, sizeof(int) * (k));
+	memcpy(nums, tmp, sizeof(int) * (numsSize));
+	free(tmp);
+	tmp = NULL;//可以不置空，出函数局部变量tmp就被销毁了
+}
