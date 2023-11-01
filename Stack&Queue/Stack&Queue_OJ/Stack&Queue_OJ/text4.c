01559bf730f9b805abe8d6c161effdfc828b665d
classmate-Han <2207946194@qq.com>
2023-11-01 08:19:42 +0800

    设计循环队列

--- Stack&Queue/Stack&Queue_OJ/Stack&Queue_OJ/text4.c
+++ Stack&Queue/Stack&Queue_OJ/Stack&Queue_OJ/text4.c
@@ -1,1 +1,69 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+//622. 设计循环队列
+//https://leetcode.cn/problems/design-circular-queue/
+
+typedef struct {
+    int front;
+    int rear;
+    int k;
+    int* a;
+} MyCircularQueue;
+
+MyCircularQueue* myCircularQueueCreate(int k) {
+    MyCircularQueue* obj = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+    obj->a = (int*)malloc((k + 1) * sizeof(int));
+    obj->k = k;
+    obj->front = obj->rear = 0;
+    return obj;
+}
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+    return obj->front == obj->rear;
+}
+
+bool myCircularQueueIsFull(MyCircularQueue* obj) {
+    return (obj->rear + 1) % (obj->k + 1) == obj->front;
+}
+
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+    if (myCircularQueueIsFull(obj)) {
+        return false;
+    }
+    obj->a[obj->rear] = value;
+    obj->rear++;
+    obj->rear %= (obj->k + 1);
+    return true;
+}
+
+bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+    if (myCircularQueueIsEmpty(obj)) {
+        return false;
+    }
+    obj->front++;
+    obj->front %= (obj->k + 1);
+    return true;
+}
+
+int myCircularQueueFront(MyCircularQueue* obj) {
+    if (myCircularQueueIsEmpty(obj)) {
+        return -1;
+    }
+    return obj->a[obj->front];
+}
+
+int myCircularQueueRear(MyCircularQueue* obj) {
+    if (myCircularQueueIsEmpty(obj)) {
+        return -1;
+    }
+    return obj->a[(obj->rear + obj->k) % (obj->k + 1)];
+}
+
+void myCircularQueueFree(MyCircularQueue* obj) {
+    free(obj->a);
+    free(obj);
+}
